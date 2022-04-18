7fd0c562a6de3549d972ec0a2a8323473b016097
Christian Valesares <cvalesares@gmail.com>
2022-04-18 13:14:02 -0400

    add testing for corridor search poro creation

--- spec/poros/hotspot_spec.rb
+++ spec/poros/hotspot_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Hotspot do
   before :each do
     @hotspot1 = MapquestFacade.search.first
+    @hotspot2 = MapquestFacade.corridor_search("39.96488,-76.729949,41.099998,-76.305603,39.899011,-76.164335,39.099998,-78.305603", 5).first
   end
 
   it 'exists', :vcr do
     expect(@hotspot1).to be_an_instance_of Hotspot
+    expect(@hotspot2).to be_an_instance_of Hotspot
   end
 
   it 'has readable attributes', :vcr do
@@ -15,5 +17,10 @@ RSpec.describe Hotspot do
     expect(@hotspot1.address).to eq("830 Allonby Rd")
     expect(@hotspot1.city).to eq("Fayetteville")
     expect(@hotspot1.state).to eq("NC")
+    expect(@hotspot2.name).to eq("Hagemeyer North America")
+    expect(@hotspot2.distance).to eq(0.0001)
+    expect(@hotspot2.address).to eq("2740 S Pleasant Valley Rd")
+    expect(@hotspot2.city).to eq("Winchester")
+    expect(@hotspot2.state).to eq("VA")
   end
 end
