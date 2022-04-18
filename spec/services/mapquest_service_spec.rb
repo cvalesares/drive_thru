c5ec57fdf8c0bcd59764298cdb24300967e8055b
Christian Valesares <cvalesares@gmail.com>
2022-04-18 13:02:40 -0400

    add corridor search endpoint to mapquest service and testing

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe MapquestService do
     expect(MapquestService.conn).to be_a Faraday::Connection
   end
 
-  it 'can find a point of interest', :vcr do
+  it 'can do a general search', :vcr do
     locations = MapquestService.search
-    
+
+    expect(locations).to be_a Hash
+    expect(locations).to have_key :searchResults
+  end
+
+  it 'can search a route for points of interest', :vcr do
+    route = "39.96488,-76.729949,41.099998,-76.305603,39.899011,-76.164335,39.099998,-78.305603"
+    locations = MapquestService.corridor_search(route, 5)
+
     expect(locations).to be_a Hash
     expect(locations).to have_key :searchResults
   end
